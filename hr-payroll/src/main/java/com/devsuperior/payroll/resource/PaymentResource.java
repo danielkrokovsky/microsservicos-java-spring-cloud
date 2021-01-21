a8526f42a585ec24b798cdcf7d92251966c73dd7
Daniel <danielkrokovsky@hotmail.com>
2021-01-20 21:57:08 -0300

    curso microservice udemy

---
+++ hr-payroll/src/main/java/com/devsuperior/payroll/resource/PaymentResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.payroll.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.payroll.entities.Payment;
+import com.devsuperior.payroll.services.PaymentService;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentResource {
+	
+	@Autowired
+	private PaymentService paymentService;
+	
+	
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+		
+		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+	}
+
+}
